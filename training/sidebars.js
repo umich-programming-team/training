c2f923b0057639b5f25cd8b60f87275f9969e700
Shang-En Huang <math.tmt514@gmail.com>
2020-09-27 15:03:45 -0400

    stl-sort

--- training/sidebars.js
+++ training/sidebars.js
@@ -9,6 +9,7 @@ module.exports = {
     "Section 1: Sorting and Searching": [
       "01-sorting-and-searching/arrays-and-containers",
       "01-sorting-and-searching/stl-sort",
+      "01-sorting-and-searching/stl-searching"
     ],
     "Section 2: Divide and Conquer": ["doc2"],
     "Section 3: Recursion and Searching Algorithms": ["doc3"]
